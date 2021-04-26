f93587ef06065f5db6534ea2da3018fddbc29c46
Marc Grec <52717034+1Radiation1@users.noreply.github.com>
2021-04-26 18:55:01 +0300

    No more .rar
    Ez

---
+++ Game/Assets/Scripts/CameraInputHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraInputHandler : MonoBehaviour
+{
+    public Camera target;
+
+    public float roll = 0.0f;
+
+    float sumX = 0.0f, sumY = 0.0f;
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X"), mouseY = Input.GetAxis("Mouse Y");
+
+        sumX = (sumX + mouseX) % 360.0f;
+
+        sumY += mouseY;
+
+        if (sumY > 90.0f)
+        {
+            sumY = 90.0f;
+        }
+        else if (sumY < -90.0f)
+        {
+            sumY = -90.0f;
+        }
+
+        target.transform.rotation = Quaternion.Euler(0.0f, 0.0f, roll) * Quaternion.Euler(-sumY, sumX, 0.0f);
+    }
+}
---
+++ Game/Assets/Scripts/PlayerMovementInputHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementInputHandler : MonoBehaviour
+{
+    public Transform lowerBound;
+    
+    public bool isTouchingGround;
+
+    public float groundCheckDistance = 0.1f;
+
+    void Update()
+    {
+        // Update isTouchingGround
+        {
+            if (Physics.Raycast(lowerBound.position, -lowerBound.up, out RaycastHit hitInfo))
+            {
+                isTouchingGround = hitInfo.distance < groundCheckDistance;
+            }
+        }
+    }
+}
