654067dc7add22501435bb5f817d525b51058834
Marc Grec <52717034+1Radiation1@users.noreply.github.com>
2021-04-29 13:54:23 +0300

    First networked version

--- Game/Assets/Scripts/PlayerMovementInputHandler.cs
+++ Game/Assets/Scripts/PlayerMovementInputHandler.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class PlayerMovementInputHandler : MonoBehaviour
+public class PlayerMovementInputHandler : NetworkBehaviour
 {
     public Rigidbody playerRb;
 
---
+++ Game/Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    public Camera playerCamera;
+
+    public GameObject movementBody;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            playerCamera.enabled = false;
+            {
+                var rb = movementBody.GetComponent<Rigidbody>();
+                rb.isKinematic = true;
+                rb.detectCollisions = rb.useGravity = false;
+            }
+            {
+                var colls = movementBody.GetComponents<Collider>();
+                foreach (var coll in colls)
+                {
+                    coll.enabled = false;
+                }
+            }
+            {
+                var pmihs = GetComponents<PlayerMovementInputHandler>();
+                foreach (var pmih in pmihs)
+                {
+                    pmih.enabled = false;
+                }
+            }
+        }
+    }
+}
